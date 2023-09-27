119b2c26e8976591b81f82c73b3a2a98b6e8b6d2
Christian Pedersen <76157202+ChrisPedersen784@users.noreply.github.com>
2023-09-27 13:50:30 +0200

    WepAPI HTTPGet & HTTPPost

---
+++ SurfBoardProject/RentalWebAPI/Model/Rental.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalWebAPI.Models
+{
+    public class Rental
+    {
+        [Key]
+        public int RentalId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
