beced56365f2951d97032e7a981d85a105cbda9c
Christian Pedersen <76157202+ChrisPedersen784@users.noreply.github.com>
2023-11-26 18:05:03 +0100

    DOne krav 9

---
+++ SurfBoardProject/Lib/Chatbot/AutoResponds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Chatbot
+{
+    public class AutoResponds
+    {
+        public async Task<string> Answer(string message)
+        {
+            string returnMessage = "";
+            if(message.ToLower() == "shipping cost")
+            {
+                returnMessage = "The shipping cost for standard delivery is $5.99.";
+            } else if(message.ToLower() == "return policy")
+            {
+                returnMessage = "Our return policy allows returns within 30 days of purchase. Please check our website for detailed information.";
+            }
+
+            return returnMessage;
+        }
+    }
+}
---
+++ SurfBoardProject/Lib/Images/ImageSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Images
+{
+    public class ImageSelector
+    {
+
+        public async Task<List<string>> imagesAsync()
+        {
+            List<string> ImageList = new List<string>();
+
+            ImageList.Add("/Funboard.jpg");
+            ImageList.Add("/Longboard.jpg");
+            ImageList.Add("/Shortboard.jpg");
+
+            return ImageList;
+        }
+    }
+}
